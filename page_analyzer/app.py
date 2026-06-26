242c00e2d1b591c1ae05b64208ee24033c48592b
Artem Cherepanov <artemcherepanov2357@gmail.com>
2026-06-26 10:19:17 +0300

    step 2

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, request, flash, redirect, url_for
 import os
 from dotenv import load_dotenv
 
@@ -10,9 +10,25 @@ app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'dev-secret-key')
 
 @app.route('/')
 def index():
-    return 'Hello, World!'
+    return render_template('index.html')
+
+
+@app.route('/urls', methods=['POST'])
+def post_url():
+    url = request.form.get('url')
+
+    if not url or not url.startswith(('http://', 'https://')):
+        flash('Некорректный URL. Введите адрес, начинающийся с http:// или https://', 'danger')
+        return render_template('index.html', url_input=url), 422
+
+    flash(f'Страница {url} добавлена', 'success')
+    return redirect(url_for('index'))
 
 
 @app.route('/health')
 def health():
     return 'OK', 200
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
